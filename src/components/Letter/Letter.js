fd57c39e21464f697b4c526bc524a2bed9d00266
SixStringsCoder <rshanlon@gmail.com>
2018-09-03 23:00:04 -0700

    Add test audio using Audio object and test letter A

--- src/components/Letter/Letter.js
+++ src/components/Letter/Letter.js
@@ -8,19 +8,28 @@ const content = {
   letters: [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
   vowels: ["a", "e", "i", "o", "u"]
 };
-
+const letter_a = new Audio(require("./audio/a_sound.m4a"));
 
 class Letter extends Component {
   state = {
-
+    letter: ''
   }
 
+  playAudio = (event) => {
+    let letter = event.target.id;
+    letter_a.crossOrigin = "anonymous";
+    letter_a.play();
+
+    this.setState({
+      letter: letter,
+    });
+  }
 
   render() {
     return (
-             content['letters'].map(letter => {
+             content['letters'].map((letter, index) => {
                 return <div className="button">
-                  <p className="letter">{letter}</p>
+                  <p className="letter" id={letter} onClick={this.playAudio}>{letter}</p>
                 </div>
               })
            );
